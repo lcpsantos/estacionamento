c1e79fa549573ac8459646cccaedbe45be0020e6
Luiz César Pacheco <lcpsantos@gmail.com>
2023-10-05 23:09:05 -0300

    Utilizando interface

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,38 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+import java.time.Duration;
+
+public class RentalService {
+
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    private TaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+
+        double hours = minutes / 60;
+
+        double basicPayment;
+        if (hours <= 12.0) {
+            basicPayment = Math.ceil(hours) * pricePerHour; // ceil arredonda para cima
+        } else {
+            basicPayment = Math.ceil(hours / 24) * pricePerDay;
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
